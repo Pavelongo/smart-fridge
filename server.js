7e05bc47591d1befd6a2fa47b1337429e7b3edcd
Pavel <pavel@Pavels-MacBook-Air.local>
2026-09-15 18:19:28 -0500

    Smart Fridge first version

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/fridge/:id", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`🧊 Smart Fridge funcionando en http://localhost:${PORT}`);
+});
+
+
+
