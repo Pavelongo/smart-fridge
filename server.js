3534d1542e837f90b4222f05908bc552b745d889
Pavel <pavel@Pavels-MacBook-Air.local>
2026-09-15 18:45:01 -0500

    Configure server port for Render

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -14,7 +15,3 @@ app.get("/fridge/:id", (req, res) => {
 app.listen(PORT, () => {
     console.log(`🧊 Smart Fridge funcionando en http://localhost:${PORT}`);
 });
-git commit -m "Smart Fridge first version"
-
-
-
